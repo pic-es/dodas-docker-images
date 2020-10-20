a427512835cbd87f63d6c28dd0d4df557face4b3
BruzzeseAgustin <bruzzese.agustin@gmail.com>
2020-10-20 10:34:32 +0200

    Modified get_token.sh token approval

--- docker/cachingondemand/xcache-escape-http/get_token.sh
+++ docker/cachingondemand/xcache-escape-http/get_token.sh
@@ -1,129 +1,37 @@
 #!/bin/bash
-set -e
-
-exit_msg() {
-  echo "Giving up as requested by user..."
-  exit 1
-}
 
 IAM_DEVICE_CODE_CLIENT_ID=a729738f-93e8-4a5f-9d58-ad6e61e4a911
-
-if [[ -z "${IAM_DEVICE_CODE_CLIENT_ID}" ]]; then
-  echo "Please set the IAM_DEVICE_CODE_CLIENT_ID env variable"
-  exit 1
-fi
-
-my_dir="$(dirname "$0")"
-echo $my_dir
-#client=$($my_dir/generate-client-secret.sh)
-
 IAM_DEVICE_CODE_CLIENT_SECRET=ANxMr_X7Lzia3C5UJmnFkQgF68TqLQPApMHDosCNJWoquHzObzc1_9ALuP6GLpzlFJcghd2KUbM5htwSrQWvTDE
 
-if [[ -z "${IAM_DEVICE_CODE_CLIENT_SECRET}" ]]; then
-  echo "Please set the IAM_DEVICE_CODE_CLIENT_SECRET env variable"
-  exit 1
-fi
-
-##IAM_DEVICE_CODE_ENDPOINT=https://dodas-iam.cloud.cnaf.infn.it/devicecode
 IAM_DEVICE_CODE_ENDPOINT=https://iam-escape.cloud.cnaf.infn.it/devicecode
-## IAM devicecode enpoint is /devicecode, e.g. https://iam.example/devicecode
-if [[ -z "${IAM_DEVICE_CODE_ENDPOINT}" ]]; then
-  echo "Please set the IAM_DEVICE_CODE_ENDPOINT env variable"
-  exit 1
-fi
-
 IAM_TOKEN_ENDPOINT=https://iam-escape.cloud.cnaf.infn.it/token
-## IAM token endpoint is /token, e.g. https://iam.example/token
-if [[ -z "${IAM_TOKEN_ENDPOINT}" ]]; then
-  echo "Please set the IAM_TOKEN_ENDPOINT env variable"
-  exit 1
-fi
-
-IAM_DEVICE_CODE_CLIENT_SCOPES=${IAM_DEVICE_CODE_CLIENT_SCOPES:-"storage.write:/magic"}
 
 response=$(mktemp)
 
-if [[ -f /tmp/refresh_token ]] && ! [[ -z /tmp/refresh_token ]]; then
-  echo "refresh toke file exist, trying to refresh"
+echo $response 
 
-echo "curl -s -f -L \
-  -u ${IAM_DEVICE_CODE_CLIENT_ID}:${IAM_DEVICE_CODE_CLIENT_SECRET} \
-  -d grant_type=refresh_token \
-  -d refresh_token=`cat  /tmp/refresh_token` \
-  -d scope='storage.write:/magic' \
-  ${IAM_TOKEN_ENDPOINT} > ${response}
-"
+IAM_DEVICE_CODE_CLIENT_SCOPES_1=${IAM_DEVICE_CODE_CLIENT_SCOPES_1:-"storage.write:/magic"}
+IAM_DEVICE_CODE_CLIENT_SCOPES_2=${IAM_DEVICE_CODE_CLIENT_SCOPES_2:-"storage.read:/magic"}
 
-  curl -q -L -s -v \
-    -u ${IAM_DEVICE_CODE_CLIENT_ID}:${IAM_DEVICE_CODE_CLIENT_SECRET} \
-    -d grant_type=refresh_token \
-    -d refresh_token=`cat  /tmp/refresh_token` ${IAM_TOKEN_ENDPOINT} 2>&1 > ${response}
-
-  if [ $? -ne 0 ]; then
-    echo "Error contacting IAM"
-    cat ${response}
-    exit 1
-  fi
-
-  error=$(jq -r .error ${response})
-  error_description=$(jq -r .error_description ${response})
-  if [[ "${error}" != "null" ]]; then
-    echo "The IAM returned the following error:"
-    echo
-    echo ${error} " " ${error_description}
-    echo
-    continue;
-  fi
-
-  access_token=$(jq -r .access_token ${response})
-
-  sed -e "s/token_template/${access_token}/" $HOME/.dodas_go_client_template.yaml > $HOME/.dodas.yaml
-  echo
-  echo
-  echo export TOKEN=${access_token}
-  exit 0
-fi
-
-echo "curl -q -f -s \
-  -u ${IAM_DEVICE_CODE_CLIENT_ID}:${IAM_DEVICE_CODE_CLIENT_SECRET} \
-  -d client_id=${IAM_DEVICE_CODE_CLIENT_ID} \
-  -d scope=\"${IAM_DEVICE_CODE_CLIENT_SCOPES}\" \
-  ${IAM_DEVICE_CODE_ENDPOINT} > ${response} "
-
-curl -q -f -s \
-  -u ${IAM_DEVICE_CODE_CLIENT_ID}:${IAM_DEVICE_CODE_CLIENT_SECRET} \
-  -d client_id=${IAM_DEVICE_CODE_CLIENT_ID} \
-  -d scope="${IAM_DEVICE_CODE_CLIENT_SCOPES}" \
-  ${IAM_DEVICE_CODE_ENDPOINT} > ${response}
-
-if [ $? -ne 0 ]; then
-  echo "Error contacting IAM"
-  cat ${response}
-  exit 1
-fi
+curl -q -f -s -u ${IAM_DEVICE_CODE_CLIENT_ID}:${IAM_DEVICE_CODE_CLIENT_SECRET} \
+  -d client_id=a729738f-93e8-4a5f-9d58-ad6e61e4a911 -d scope=\"${IAM_DEVICE_CODE_CLIENT_SCOPES_1}\" -d scope=\"${IAM_DEVICE_CODE_CLIENT_SCOPES_2}\" ${IAM_DEVICE_CODE_ENDPOINT} > ${response}
 
 device_code=$(jq -r .device_code ${response})
 user_code=$(jq -r .user_code ${response})
 verification_uri=$(jq -r .verification_uri ${response})
+verification_uri_complete=$(jq -r .verification_uri_complete ${response})
 expires_in=$(jq -r .expires_in ${response})
-
-trap "exit_msg" INT
 
 echo "Please open the following URL in the browser:"
 echo
-
-echo ${verification_uri}
+echo ${verification_uri_complete}
 echo
-
 echo "and, after having been authenticated, enter the following code when requested:"
 echo
-
 echo ${user_code}
-
 echo
 echo "Note that the code above expires in ${expires_in} seconds..."
 echo "Once you have correctly authenticated and authorized this device, this script can be restarted to obtain a token. "
-
 
 while true; do
 
@@ -135,7 +43,7 @@ while true; do
     [[ $a = "n" || $a = "N" ]] && exit 0
   done
 
-  curl -q -L -s \
+ curl -q -L -s \
     -u ${IAM_DEVICE_CODE_CLIENT_ID}:${IAM_DEVICE_CODE_CLIENT_SECRET} \
     -d grant_type=urn:ietf:params:oauth:grant-type:device_code \
     -d device_code=${device_code} ${IAM_TOKEN_ENDPOINT} 2>&1 > ${response}
@@ -180,3 +88,4 @@ while true; do
   exit 0
 
 done
+
